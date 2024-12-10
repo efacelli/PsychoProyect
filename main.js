70321db7bbdf3e5b7637badf18467c67e8b0366f
efacelli <enzomagic32@gmail.com>
2024-12-10 13:33:56 -0300

    script

---
+++ main.js
@@ -0,0 +1,17 @@
+document.querySelectorAll('#navbar a[href^="#"]').forEach(anchor => {
+    // Agregar event listener para el clic
+    anchor.addEventListener('click', function(e) {
+        // Prevenir el comportamiento predeterminado del enlace
+        e.preventDefault();
+        
+        // Obtener el destino del enlace (la sección)
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+        
+        // Realizar el desplazamiento suave
+        targetSection.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+});
